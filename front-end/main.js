99f0916454fc0aa90cba16dd8bf86cad52b147ce
cedric kadima <kadimacedric3@gmail.com>
2023-02-03 14:35:16 +0100

    premier renvoi de données a partir du server.py

--- front-end/main.js
+++ front-end/main.js
@@ -1,6 +1,13 @@
 const reponse = document.getElementById('answer1');
+const start = document.getElementById('start');
 const socket = new WebSocket('ws://localhost:8765');
 
+start.addEventListener('click', function () {
+        console.log('clickStart');
+        socket.send('startGame');
+        start.style.display = 'none';
+    }
+);
 reponse.addEventListener('click', function () {
     console.log('clickChoice');
     socket.send('choicePlayer');
